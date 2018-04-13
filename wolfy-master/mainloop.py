7348f47536cddaee1dafb2e603d9a742f2b2e83b
ElectroCute13 <darshgajjar@live.com>
2018-04-14 01:07:33 +0530

    Some updates

--- wolfy-master/mainloop.py
+++ wolfy-master/mainloop.py
@@ -13,6 +13,6 @@ def mainloop(j, sx, ex, srange, erange, ups, ls, a, b):
     te3, te4 = l.move(k2, y2)
     alpha = mapyy(te3, 0, 180, ups.sp, ups.ep)
     beta = mapyy(te4 - 90, 0, 180, ls.sp, ls.ep)
-    pwm.set_pwm(a, 0, alpha)
-    pwm.set_pwm(b, 0, beta)
+    pwm.set_pwm(a, 0, int(alpha))
+    pwm.set_pwm(b, 0, int(beta))
     return alpha, beta
